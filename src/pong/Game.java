3ec068091a34f134b39cd38603b801c513309bcd
Daniel Melo <danielfarias380@gmail.com>
2020-07-30 23:03:19 -0300

    Render strategy and game loop

--- src/pong/Game.java
+++ src/pong/Game.java
@@ -2,6 +2,9 @@ package pong;
 
 import java.awt.Canvas;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
 
@@ -12,8 +15,14 @@ public class Game extends Canvas implements Runnable {
 	public static int HEIGHT = 120;
 	public static int SCALE = 3;
 	
+	public BufferedImage layer = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+	
+	public Player player;
+	
 	public Game() {
 		this.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+		
+		player = new Player();
 	}
 	
 	public static void main(String[] args) {
@@ -25,11 +34,43 @@ public class Game extends Canvas implements Runnable {
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
+		
+		new Thread(game).start();
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if (bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = layer.getGraphics();
+		player.render(g);
+		
+		g = bs.getDrawGraphics();
+		g.drawImage(layer, 0, 0, WIDTH*SCALE, HEIGHT*SCALE, null);
+		
+		bs.show();
 	}
 	
 	@Override
 	public void run() {
 		requestFocus();
+		while(true) {
+			tick();
+			render();
+			try {
+				Thread.sleep(1000/60);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		}
 	}
 	
 }
