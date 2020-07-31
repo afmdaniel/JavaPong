f48b765f9f343b3a97dcbd071a2e4317db3da978
Daniel Melo <danielfarias380@gmail.com>
2020-07-30 23:30:18 -0300

    Finish player logic

--- src/pong/Player.java
+++ src/pong/Player.java
@@ -5,12 +5,33 @@ import java.awt.Graphics;
 
 public class Player {
 	
+	public boolean right, left;
+	
+	public int x, y, width, height;
+	
+	public Player(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.width = 40;
+		this.height = 10;
+	}
+	
 	public void tick() {
+		if (right) {
+			x += 2;
+		} else if (left) {
+			x -= 2;
+		}
 		
+		if (x + width > Game.WIDTH) {
+			x = Game.WIDTH - width;
+		} else if (x < 0) {
+			x = 0;
+		}
 	}
 	
 	public void render(Graphics g) {
 		g.setColor(Color.WHITE);
-		g.fillRect(100, 120 - 10, 40, 10);
+		g.fillRect(x, Game.HEIGHT - height, width, height);
 	}
 }
