dc600208adc913d52f74a3711e3323576ae26de8
Daniel Melo <danielfarias380@gmail.com>
2020-08-01 10:26:36 -0300

    Ball move

--- src/pong/Ball.java
+++ src/pong/Ball.java
@@ -2,25 +2,33 @@ package pong;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 
 public class Ball {
 	
-	public int x, y;
+	public double x, y;
 	public int width, height;
+	
+	public double dx, dy;
+	public double speed = 2;
 	
 	public Ball(int x, int y) {
 		this.x = x;
 		this.y = y;
 		this.width = 5;
 		this.height = 5;
+		
+		this.dx = new Random().nextGaussian();
+		this.dy = new Random().nextGaussian();
 	}
 	
 	public void tick() {
-		
+		x += dx*speed;
+		y += dy*speed;
 	}
 	
 	public void render(Graphics g) {
 		g.setColor(Color.white);
-		g.fillRect(x, y, width, height);
+		g.fillRect((int) x,(int) y, width, height);
 	}
 }
